12ed550eeb668a2a6b2fdc2b44fa4071442fda1f
kauanhen02 <139480381+kauanhen02@users.noreply.github.com>
2023-09-25 14:55:03 -0300

    Update router.js

--- js/router.js
+++ js/router.js
@@ -1,55 +1,27 @@
-class Router {
-  constructor() {
-    this.routes = {};
-    this.currentRoute = null;
-    this.appElement = document.querySelector('#app');
-    window.addEventListener('popstate', () => this.handle());
-  }
+export class Router {
+  routes = {} 
 
   add(routeName, page) {
-    this.routes[routeName] = page;
+    this.routes[routeName] = page
   }
+  
+  route(event) {
+    event = event || window.event
+    event.preventDefault()
 
-  navigateTo(route) {
-    window.history.pushState({}, '', route);
-    this.handle();
+    window.history.pushState({}, "", event.target.href)
+
+    this.handle()
   }
 
   handle() {
-    const { pathname } = window.location;
-    const route = this.routes[pathname] || this.routes['404'];
-
-    // Fetch the page content
+    const { pathname }  = window.location
+    const route = this.routes[pathname] || this.routes[404]
     fetch(route)
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.text();
-      })
-      .then((html) => {
-        this.currentRoute = route;
-        this.appElement.innerHTML = html;
-      })
-      .catch((error) => console.error('Error fetching page:', error));
+    .then(data => data.text())
+    .then(html => {
+      document.querySelector('#app').innerHTML = html
+    })
   }
+
 }
-
-const router = new Router();
-
-// Example usage
-router.add('/', 'home.html');
-router.add('/about', 'about.html');
-router.add('404', '404.html');
-
-// Handle initial page load
-window.addEventListener('load', () => router.handle());
-
-// Handle navigation
-document.addEventListener('click', (event) => {
-  const target = event.target;
-  if (target.tagName === 'A' && target.getAttribute('href')) {
-    event.preventDefault();
-    router.navigateTo(target.getAttribute('href'));
-  }
-});
