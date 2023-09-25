55fb6a9c734909077738a50537e32238881d6e2b
kauanhen02 <139480381+kauanhen02@users.noreply.github.com>
2023-09-25 14:59:20 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -10,3 +10,19 @@ router.handle()
 
 window.onpopstate = () => router.handle()
 window.route = () => router.route()
+
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor iniciado na porta ${PORT}`);
+});
